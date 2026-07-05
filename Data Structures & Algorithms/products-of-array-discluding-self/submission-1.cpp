efbc0dc4142ba3c02820ad03cb4797bb04f9c9a4
DarshanShivaraj77 <146374507+DarshanShivaraj77@users.noreply.github.com>
2026-07-05 21:24:03 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // brute force method:
+        // vector<int> ans(nums.size(),1);
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=0;j<nums.size();i++){
+        //         if(i!=j){
+        //             ans[i]*=nums[j];
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // optimal approach:
+        int n=nums.size();
+        vector<int> ans(n,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int suffix=1;
+        for(int i=n-2;i>=0;i--){
+            suffix*=nums[i+1];
+            ans[i]*=suffix;
+        }
+        return ans;
+    }
+};
